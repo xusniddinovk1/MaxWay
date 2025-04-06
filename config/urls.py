dcee30dec6ec80a07c408f5ed37be3b6649b95ee
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-06 19:02:40 +0500

    update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('food.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
