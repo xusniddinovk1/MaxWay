c2f7ea9b1a82830388a525693b3a8de0e72741ef
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-06 19:08:02 +0500

    update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login_required_decorator(func):
+    return login_required(func, login_url="login_page")
+
+
+def login_page(request):
+    if request.POST:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("main_dashboard")
+        return render(request, "dashboard/login.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("login_page")
