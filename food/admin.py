1c355bc336d87f55d1b7191384bffe28ac56b0a6
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-13 16:22:19 +0500

    initial commit

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from food.models import Category, Order, OrderProduct, Customer, Product
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Customer)
+admin.site.register(Product)
