b3a937210037a382c725b84d3dd3e5d3b39b41a9
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-13 16:03:57 +0500

    initial commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from . import services
-from forms import *
+from .forms import *
 
 
 def login_required_decorator(func):
