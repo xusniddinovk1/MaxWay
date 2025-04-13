b39ed57f4e99e6eefd297801e13f3d48888d5bd9
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-13 16:32:21 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('food.urls')),
+    path('', include('food.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
 
